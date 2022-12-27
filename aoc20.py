5eba716ad62d9d88667e96a7e297af5b4f23e03f
Viljar Femoen <viljar.femoen@hotmail.no>
2022-12-27 10:39:45 +0100

    2 star 20

--- aoc20.py
+++ aoc20.py
@@ -1,41 +1,53 @@
 
-fasit = iter([
-    [2, 1, -3, 3, -2, 0, 4],
-    [1, -3, 2, 3, -2, 0, 4],
-    [1, 2, 3, -2, -3, 0, 4],
-    [1, 2, -2, -3, 0, 3, 4],
-    [1, 2, -3, 0, 3, 4, -2],
-    [1, 2, -3, 0, 3, 4, -2],
-    [1, 2, -3, 4, 0, 3, -2]
-])
-
 with open("input20.txt", "r") as f:
-    data = [int(l) for l in f]
+    data = [l.strip() for l in f]
+counts = {}
+for i, l in enumerate(data):
+    count = counts.get(l, 0)
+    data[i] += f"|{count}"
+    counts[l] = count + 1
 
 l = len(data)
+# print(len(set(data)))
+# fuck me, the elements are not unique
+# I need another way to get i below
+og = data.copy()
+def mix(data):
+    for n in og:
+        i = data.index(n) 
+        _n = n
+        n = (int(n.split("|")[0]) % (l - 1))
 
-for n in data.copy():
-    i = data.index(n)
-    if n < 0:
+        if n < 0:
+            data = data[i:] + data[:i]
+            data.append(data.pop(0))
+            data[n-1:] = [_n] + data[n-1:-1]
+
+        elif n > 0:
+            data = data[i:] + data[:i]   
+            data[:n+1] = data[1:n+1] + [_n]
+
+        i = data.index("0|0")
         data = data[i:] + data[:i]
         data.append(data.pop(0))
-
-        data[n-1:] = [n] + data[n-1:-1]
-
-    elif n > 0:
-        data = data[i:] + data[:i]   
-
-        data[:n+1] = data[1:n+1] + [n]
-
-    # el = next(fasit)
-    # while data[0] != el[0]:
-    #     data.append(data.pop(0))
-    # print("sort")
-    # print(data)
-    # print(el)
-    # print()
-        
-ind = data.index(0)
+        data.append(data.pop(0))
+        data.append(data.pop(0))
+        data.append(data.pop(0))
+    return data
+data = mix(data)
+ind = data.index("0|0")
 ans = [data[(ind + i) % l] for i in [1000, 2000, 3000]]
+
 print(ans)
-print(sum(ans))
+print(sum(int(i.split("|")[0]) for i in ans))
+
+og = [str(811589153 * int(i.split("|")[0])) + "|" + i.split("|")[1] for i in og]
+data = og.copy()
+for _ in range(10):
+    print(_)
+    data = mix(data)
+ind = data.index("0|0")
+ans = [data[(ind + i) % l] for i in [1000, 2000, 3000]]
+
+print(ans)
+print(sum(int(i.split("|")[0]) for i in ans))
